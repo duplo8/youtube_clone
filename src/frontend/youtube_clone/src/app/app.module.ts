6b4c661db508f2d2ca590de879461edd0a61b297
duplo8 <MGR.rupel@gmail.com>
2023-10-12 14:28:21 +0200

    added routing

--- src/frontend/youtube_clone/src/app/app.module.ts
+++ src/frontend/youtube_clone/src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainPageComponent } from './main-page/main-page.component';
 import { VideoPageComponent } from './video-page/video-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     VideoPageComponent,
-    ProfilePageComponent
+    ProfilePageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    RouterModule.forRoot([
+      {path: '', component: MainPageComponent},
+      {path: 'video-page', component: VideoPageComponent},
+      {path: 'profile-page', component: ProfilePageComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
