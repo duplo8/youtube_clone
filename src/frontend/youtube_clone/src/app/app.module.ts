e4117fbe2b7022bca436c28185a5fa06a683dd39
duplo8 <MGR.rupel@gmail.com>
2023-10-13 13:37:59 +0200

    Improved and cleaned up

--- src/frontend/youtube_clone/src/app/app.module.ts
+++ src/frontend/youtube_clone/src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainPageComponent } from './main-page/main-page.component';
-import { VideoPageComponent } from './video-page/video-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { RouterModule } from '@angular/router';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { VideoComponent } from './video/video.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    VideoPageComponent,
-    ProfilePageComponent,
+    VideoComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,14 +26,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     BrowserAnimationsModule,
     MatSlideToggleModule,
     MatSidenavModule,
-    MatButtonModule,
-    RouterModule.forRoot([      
-      {path: '', redirectTo: 'main-page', pathMatch: 'full'},
-      {path: 'main-page', component: MainPageComponent},
-      {path: 'video-page', component: VideoPageComponent},
-      {path: 'profile-page', component: ProfilePageComponent},
-      {path: '', redirectTo: 'main-page', pathMatch: 'full'}
-    ])
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
